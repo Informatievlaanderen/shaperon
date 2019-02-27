c894472a00d3814dcd21b5534371bd2f89352fda
Yves Reynhout <yves.reynhout@gmail.com>
2019-02-27 17:32:14 +0100

    feat: enumerators for shape and dbase records

--- src/Be.Vlaanderen.Basisregisters.Shaperon/DbaseRecordCount.cs
+++ src/Be.Vlaanderen.Basisregisters.Shaperon/DbaseRecordCount.cs
@@ -1,9 +1,14 @@
 namespace Be.Vlaanderen.Basisregisters.Shaperon
 {
     using System;
+    using System.Data.Common;
     using System.Diagnostics.Contracts;
 
-    public readonly struct DbaseRecordCount : IEquatable<DbaseRecordCount>
+    public readonly struct DbaseRecordCount :
+        IEquatable<DbaseRecordCount>,
+        IComparable<DbaseRecordCount>,
+        IEquatable<RecordNumber>,
+        IComparable<RecordNumber>
     {
         private readonly int _value;
 
@@ -17,12 +22,53 @@ namespace Be.Vlaanderen.Basisregisters.Shaperon
         }
 
         public bool Equals(DbaseRecordCount other) => _value.Equals(other._value);
-        public override bool Equals(object obj) => obj is DbaseRecordCount length && Equals(length);
+
+        public int CompareTo(DbaseRecordCount other)
+        {
+            return _value.CompareTo(other._value);
+        }
+
+        public bool Equals(RecordNumber other) => _value.Equals(other.ToInt32());
+
+        public int CompareTo(RecordNumber other)
+        {
+            return _value.CompareTo(other.ToInt32());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is DbaseRecordCount length && Equals(length))
+            {
+                return true;
+            }
+
+            if (obj is RecordNumber number && Equals(number))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         public override int GetHashCode() => _value;
 
         [Pure]
         public int ToInt32() => _value;
 
         public override string ToString() => _value.ToString();
+
+        public static bool operator ==(DbaseRecordCount left, DbaseRecordCount right) => left.Equals(right);
+        public static bool operator !=(DbaseRecordCount left, DbaseRecordCount right) => !left.Equals(right);
+        public static bool operator <=(DbaseRecordCount left, DbaseRecordCount right) => left.CompareTo(right) <= 0;
+        public static bool operator <(DbaseRecordCount left, DbaseRecordCount right) => left.CompareTo(right) < 0;
+        public static bool operator >=(DbaseRecordCount left, DbaseRecordCount right) => left.CompareTo(right) >= 0;
+        public static bool operator >(DbaseRecordCount left, DbaseRecordCount right) => left.CompareTo(right) > 0;
+
+        public static bool operator ==(DbaseRecordCount left, RecordNumber right) => left.Equals(right);
+        public static bool operator !=(DbaseRecordCount left, RecordNumber right) => !left.Equals(right);
+        public static bool operator <=(DbaseRecordCount left, RecordNumber right) => left.CompareTo(right) <= 0;
+        public static bool operator <(DbaseRecordCount left, RecordNumber right) => left.CompareTo(right) < 0;
+        public static bool operator >=(DbaseRecordCount left, RecordNumber right) => left.CompareTo(right) >= 0;
+        public static bool operator >(DbaseRecordCount left, RecordNumber right) => left.CompareTo(right) > 0;
     }
 }
---
+++ test/Be.Vlaanderen.Basisregisters.Shaperon.Tests/ShapeRecordEnumeratorWithShorterStreamTests.cs
@@ -0,0 +1,81 @@
+namespace Be.Vlaanderen.Basisregisters.Shaperon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using AutoFixture;
+    using Xunit;
+
+    public class ShapeRecordEnumeratorWithShorterStreamTests
+    {
+        private readonly Fixture _fixture;
+        private readonly IEnumerator<ShapeRecord> _sut;
+        private readonly DisposableBinaryReader _reader;
+        private readonly ShapeRecord _record;
+
+        public ShapeRecordEnumeratorWithShorterStreamTests()
+        {
+            _fixture = new Fixture();
+            _fixture.CustomizeShapeRecordCount();
+            _fixture.CustomizeWordLength();
+
+            var content = new PointShapeContent(new PointM(1.0, 1.0));
+            var number = RecordNumber.Initial;
+            _record = content.RecordAs(number);
+            var header = new ShapeFileHeader(
+                ShapeFileHeader.Length.Plus(_record.Length.Times(2)),
+                ShapeType.Point,
+                _fixture.Create<BoundingBox3D>());
+            var stream = new MemoryStream();
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                header.Write(writer);
+                _record.Write(writer);
+                writer.Write(_fixture.CreateMany<byte>(_record.Length.ToByteLength().ToInt32() / 2).ToArray());
+                writer.Flush();
+            }
+
+            stream.Position = 100;
+
+            _reader = new DisposableBinaryReader(stream, Encoding.UTF8, false);
+            _sut = header.CreateShapeRecordEnumerator(_reader);
+        }
+
+        [Fact]
+        public void MoveNextReturnsExpectedResult()
+        {
+            Assert.True(_sut.MoveNext());
+            Assert.ThrowsAny<Exception>(() => _sut.MoveNext());
+        }
+
+        [Fact]
+        public void CurrentReturnsExpectedResult()
+        {
+            Assert.Throws<InvalidOperationException>(() => _sut.Current);
+
+            _sut.MoveNext();
+
+            Assert.Equal(_record, _sut.Current, new ShapeRecordEqualityComparer());
+
+            Assert.ThrowsAny<Exception>(() => _sut.MoveNext());
+
+            Assert.Throws<InvalidOperationException>(() => _sut.Current);
+        }
+
+        [Fact]
+        public void ResetReturnsExpectedResult()
+        {
+            Assert.Throws<NotSupportedException>(() => _sut.Reset());
+        }
+
+        [Fact]
+        public void DisposeHasExpectedResult()
+        {
+            Assert.False(_reader.Disposed);
+            _sut.Dispose();
+            Assert.True(_reader.Disposed);
+        }
+    }
+}
