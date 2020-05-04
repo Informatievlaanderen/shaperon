4b45710f6db00a764a6269dde75f5a8d940adc39
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-05-04 17:15:24 +0200

    fix: move to GH-actions

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 version 5.241.6
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 3.3.2 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.0.6 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
@@ -18,10 +18,10 @@ let build = buildSolution assemblyVersionNumber
 let publish = publishSolution assemblyVersionNumber
 let pack = packSolution nugetVersionNumber
 
+supportedRuntimeIdentifiers <- [ "linux-x64" ]
+
 // Library ------------------------------------------------------------------------
-
 Target.create "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.Shaperon")
-
 Target.create "Lib_Test" (fun _ ->
   [
     "test" @@ "Be.Vlaanderen.Basisregisters.Shaperon.Tests" ]
@@ -29,23 +29,24 @@ Target.create "Lib_Test" (fun _ ->
 )
 
 Target.create "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.Shaperon")
-
 Target.create "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.Shaperon")
 
 // --------------------------------------------------------------------------------
-
-Target.create "PublishLibrary" ignore
 Target.create "PublishAll" ignore
-
-Target.create "PackageMyGet" ignore
 Target.create "PackageAll" ignore
 
 // Publish ends up with artifacts in the build folder
-"DotNetCli" ==> "Clean" ==> "Restore" ==> "Lib_Build" ==> "Lib_Test" ==> "Lib_Publish" ==> "PublishLibrary"
-"PublishLibrary" ==> "PublishAll"
+"DotNetCli"
+==> "Clean"
+==> "Restore"
+==> "Lib_Build"
+==> "Lib_Test"
+==> "Lib_Publish"
+==> "PublishAll"
 
 // Package ends up with local NuGet packages
-"PublishLibrary" ==> "Lib_Pack" ==> "PackageMyGet"
-"PackageMyGet" ==> "PackageAll"
+"PublishAll"
+==> "Lib_Pack"
+==> "PackageAll"
 
 Target.runOrDefault "Lib_Test"
