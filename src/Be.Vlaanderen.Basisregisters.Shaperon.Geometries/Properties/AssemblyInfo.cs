76a1d87e7aaf51f8b1ff3d6cb839ba6dbb34f71c
jvandaal <jonas.vandaal@outlook.be>
2024-03-07 17:10:51 +0100

    chore: add assembly info to geometries proj

---
+++ src/Be.Vlaanderen.Basisregisters.Shaperon.Geometries/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Vlaamse basisregisters")]
+[assembly: AssemblyDescription("Lightweight dbase and shape record handling (net topology suite integration)")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("7d166fa0-4c2b-45de-817f-24cac544e37f")]
