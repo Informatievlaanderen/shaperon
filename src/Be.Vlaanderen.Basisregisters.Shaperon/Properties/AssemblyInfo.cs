3aefdba112b3d8724fb01c4fa24b502039c4ac4f
jvandaal <jonas.vandaal@outlook.be>
2024-03-07 16:36:35 +0100

    feat: move to dotnet 8.0.2
    move to dotnet 8.0.2
    
    BREAKING CHANGE: move to dotnet 8.0.2

--- src/Be.Vlaanderen.Basisregisters.Shaperon/Properties/AssemblyInfo.cs
+++ src/Be.Vlaanderen.Basisregisters.Shaperon/Properties/AssemblyInfo.cs
@@ -4,8 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("Vlaamse basisregisters")]
 [assembly: AssemblyDescription("Lightweight dbase and shape record handling")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
 
